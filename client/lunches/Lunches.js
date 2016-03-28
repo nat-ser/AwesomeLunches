c8f7749fb5601768d5c5c7b80ddc91d7dbbbcabd
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2016-03-27 20:15:33 -0400

    add template subscriptions

--- client/lunches/Lunches.js
+++ client/lunches/Lunches.js
@@ -1,1 +1,12 @@
-Meteor.subscribe('lunches');
+
+Template.Lunches.onCreated(function(){
+  var self= this;
+  self.autorun(function(){
+    self.subscribe('lunches');
+  });
+});
+Template.Lunches.helpers({
+  lunches: ()=> {
+    return Lunches.find({});
+  }
+});
