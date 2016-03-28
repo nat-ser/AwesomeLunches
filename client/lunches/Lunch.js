ff243e4caed8fd82bb5e1aff77b7a7648bdb5d50
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2016-03-27 23:46:01 -0400

    add edit form to lunches

--- client/lunches/Lunch.js
+++ client/lunches/Lunch.js
@@ -1,3 +1,9 @@
+Template.Lunch.helpers({
+  updateLunchId: function(){
+    return this._id;
+  }
+});
+
 Template.Lunch.events({
   'click .toggle-menu': function(){
     Meteor.call('toggleMenuItem', this._id, this.inMenu);
